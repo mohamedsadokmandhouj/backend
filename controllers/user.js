173f96bdfd4a20327eb375cea73cc36e690d9e3b
mohamed sadok mandhouj <sadek.mandhouj04@gmal.com>
2024-10-31 14:26:19 +0100

    adding backend files

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 
 import bcrypt from 'bcrypt'
-import Users from '../models/Users.js';
+import Users from '../models/users.js';
 
 import asyncHandler from 'express-async-handler'
 import { generateAccessToken } from '../utils/authMiddleware.js';
